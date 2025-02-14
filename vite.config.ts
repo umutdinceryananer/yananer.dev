48360f64aa8428e810e7919768369276b1951e45
Umut Dinçer Yananer <umutdncr@gmail.com>
2025-02-14 17:28:48 +0300

    fix: Complete overhaul of build and deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,35 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  assetsInclude: ['**/*.md'],
-  base: './',
-  define: {
-    'process.env': {},
-    Buffer: ['buffer', 'Buffer'],
-  },
-  optimizeDeps: {
-    include: ['buffer'],
-  },
+  base: '',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true,
   },
 })
