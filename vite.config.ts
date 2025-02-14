3327eee9c47a2367a1baa68c0c416f6b671e7a3d
Umut Dinçer Yananer <umutdncr@gmail.com>
2025-02-14 17:07:53 +0300

    Configure Github Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.md'],
+  base: '/',
   define: {
     'process.env': {},
     Buffer: ['buffer', 'Buffer'],
